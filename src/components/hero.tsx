ecb04b85600f3565f78c71ac59f4cd6ba0580c3d
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-23 15:12:09 +0000

    displayed images in hero.tsx xomponent

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,19 @@
+import typewriter from "../images/typewriter.png";
+import paper1 from "../images/paper1.png";
+import paper2 from "../images/paper2.png";
+import paper3 from "../images/paper3.png";
+import paper4 from "../images/paper4.png";
+import { type } from "os";
+
 const Hero = () => {
-  return <h1>HERO</h1>;
+  return (
+    <div>
+      <img src={typewriter} />
+      <img src={paper1} />
+      <img src={paper2} />
+      <img src={paper3} />
+      <img src={paper4} />
+    </div>
+  );
 };
 export default Hero;
