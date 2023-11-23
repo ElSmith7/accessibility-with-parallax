4625a06ecdfd44e14552488cbee91f329a0f1f46
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-23 15:41:25 +0000

    tailwind set up and configuration
    Using create-react app. Use CLI in future but current version will suffice.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,6 @@ import paper1 from "../images/paper1.png";
 import paper2 from "../images/paper2.png";
 import paper3 from "../images/paper3.png";
 import paper4 from "../images/paper4.png";
-import { type } from "os";
 
 const Hero = () => {
   return (
