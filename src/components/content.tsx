56ef13c0386c43ad25a4e5846f6d82f5ed909a13
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-25 19:02:57 +0000

    Moved dark tear img into content.tsx
    The movement of the paper tear divider doesn't interfere with the content which scrolls at the same rate as the user!

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,22 +1,36 @@
+import { Parallax } from "react-scroll-parallax";
+import dark_tear from "../images/dark_tear.png";
+
 const Content = () => {
   return (
-    <div className="bg-black mt-10 text-white z-20">
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2> <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2> <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2> <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2>
-      <h2>Content</h2> <h2>Content</h2>
-      <h2>Content</h2>
+    <div className="relative">
+      <Parallax speed={24}>
+        <img
+          className="absolute w-full -mt-32"
+          src={dark_tear}
+          alt="paper tear"
+        />
+        <div className="p-32 bg-black" />
+      </Parallax>
+
+      <div className=" relative bg-black mt-10 text-white z-10 -mt-32">
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2> <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2> <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2> <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2>
+        <h2>Content</h2> <h2>Content</h2>
+        <h2>Content</h2>
+      </div>
     </div>
   );
 };
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,7 +6,6 @@ import paper1 from "../images/paper1.png";
 import paper2 from "../images/paper2.png";
 import paper3 from "../images/paper3.png";
 import paper4 from "../images/paper4.png";
-import dark_tear from "../images/dark_tear.png";
 
 const Hero = () => {
   const parallax0 = useParallax<HTMLDivElement>({ speed: -2 });
@@ -14,7 +13,6 @@ const Hero = () => {
   const parallax2 = useParallax<HTMLDivElement>({ speed: 5 });
   const parallax3 = useParallax<HTMLDivElement>({ speed: 12 });
   const parallax4 = useParallax<HTMLDivElement>({ speed: 15 });
-  const parallax5 = useParallax<HTMLDivElement>({ speed: 20 });
 
   const parallaxController = useParallaxController();
 
@@ -45,13 +43,6 @@ const Hero = () => {
         <div ref={parallax4.ref} className="absolute">
           <img src={paper4} alt="crumpled paper sketch" />
         </div>
-      </div>
-      <div ref={parallax5.ref}>
-        <img
-          className="flex w-full z-10 -mt-32"
-          src={dark_tear}
-          alt="paper tear"
-        />
       </div>
     </div>
   );
