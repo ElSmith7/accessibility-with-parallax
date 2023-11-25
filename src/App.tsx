e760b14064288f00bd4ff97bec552c33db4dcbd3
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-25 17:36:51 +0000

    Created dummy content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ParallaxProvider } from "react-scroll-parallax";
 import Hero from "./components/hero";
+import Content from "./components/content";
 
 const App = () => {
   return (
     <ParallaxProvider>
       <Hero />
+      <Content />
     </ParallaxProvider>
   );
 };
---
+++ src/components/content.tsx
@@ -0,0 +1,23 @@
+const Content = () => {
+  return (
+    <div className="bg-black mt-10 text-white z-20">
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2> <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2> <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2> <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2>
+      <h2>Content</h2> <h2>Content</h2>
+      <h2>Content</h2>
+    </div>
+  );
+};
+export default Content;
