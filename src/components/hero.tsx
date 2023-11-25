f6cef3f6c4262e3f7fc8c2099e2c8e925ac74747
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-25 17:37:56 +0000

    experimenting with hero to content transition

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,6 +6,7 @@ import paper1 from "../images/paper1.png";
 import paper2 from "../images/paper2.png";
 import paper3 from "../images/paper3.png";
 import paper4 from "../images/paper4.png";
+import dark_tear from "../images/dark_tear.png";
 
 const Hero = () => {
   const parallax0 = useParallax<HTMLDivElement>({ speed: -2 });
@@ -13,6 +14,8 @@ const Hero = () => {
   const parallax2 = useParallax<HTMLDivElement>({ speed: 5 });
   const parallax3 = useParallax<HTMLDivElement>({ speed: 12 });
   const parallax4 = useParallax<HTMLDivElement>({ speed: 15 });
+  const parallax5 = useParallax<HTMLDivElement>({ speed: 20 });
+
   const parallaxController = useParallaxController();
 
   useEffect(() => {
@@ -22,24 +25,33 @@ const Hero = () => {
   }, [parallaxController]);
 
   return (
-    <div
-      className="relative flex justify-center items-center"
-      style={{ height: "620px" }}
-    >
-      <div ref={parallax0.ref} className="absolute">
-        <img src={typewriter} alt="typewriter sketch" />
+    <div>
+      <div
+        className="relative flex justify-center items-center"
+        style={{ height: "620px" }}
+      >
+        <div ref={parallax0.ref} className="absolute">
+          <img src={typewriter} alt="typewriter sketch" />
+        </div>
+        <div ref={parallax1.ref} className="absolute">
+          <img src={paper1} alt="crumpled paper sketch" />
+        </div>
+        <div ref={parallax2.ref} className="absolute">
+          <img src={paper2} alt="crumpled paper sketch" />
+        </div>
+        <div ref={parallax3.ref} className="absolute">
+          <img src={paper3} alt="crumpled paper sketch" />
+        </div>
+        <div ref={parallax4.ref} className="absolute">
+          <img src={paper4} alt="crumpled paper sketch" />
+        </div>
       </div>
-      <div ref={parallax1.ref} className="absolute">
-        <img src={paper1} alt="crumpled paper sketch" />
-      </div>
-      <div ref={parallax2.ref} className="absolute">
-        <img src={paper2} alt="crumpled paper sketch" />
-      </div>
-      <div ref={parallax3.ref} className="absolute">
-        <img src={paper3} alt="crumpled paper sketch" />
-      </div>
-      <div ref={parallax4.ref} className="absolute">
-        <img src={paper4} alt="crumpled paper sketch" />
+      <div ref={parallax5.ref}>
+        <img
+          className="flex w-full z-10 -mt-32"
+          src={dark_tear}
+          alt="paper tear"
+        />
       </div>
     </div>
   );
