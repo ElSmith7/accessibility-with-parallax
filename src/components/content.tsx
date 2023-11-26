2d8135f5ec685da7eb204b2521288b16a30408ac
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-26 14:37:44 +0000

    Updated styling

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,16 +4,12 @@ import dark_tear from "../images/dark_tear.png";
 const Content = () => {
   return (
     <div className="relative">
-      <Parallax speed={24}>
-        <img
-          className="absolute w-full -mt-32"
-          src={dark_tear}
-          alt="paper tear"
-        />
-        <div className="p-32 bg-black" />
+      <Parallax speed={20}>
+        <img className="w-full -mt-40" src={dark_tear} alt="paper tear" />
+        <div className="p-28 bg-black" />
       </Parallax>
 
-      <div className=" relative bg-black mt-10 text-white z-10 -mt-32">
+      <div className=" relative bg-black text-white z-10 -mt-40 pt-32   ">
         <h2>Content</h2>
         <h2>Content</h2>
         <h2>Content</h2> <h2>Content</h2>
