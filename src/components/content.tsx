ec06d52697405740d70c34144a44479372eabce3
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-27 18:17:30 +0000

    added new section to content
    again, contains place holder text

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,19 +1,22 @@
 import { Parallax } from "react-scroll-parallax";
 import dark_tear from "../images/dark_tear.png";
+import light_tear from "../images/light_tear.png";
 
 const Content = () => {
   return (
     <div className="relative">
       <Parallax speed={20}>
         <img className="w-full -mt-40" src={dark_tear} alt="paper tear" />
-        <div className=" p-28 bg-black" />
+        <div className="p-28 bg-black" />
       </Parallax>
 
-      <div className="relative bg-black text-white z-10 -mt-40 py-32">
-        <div className="font-arimo mx-24">
-          <h1 className="font-bold text-4xl">Intro</h1>
+      <div className="relative bg-black text-white z-1 -mt-40 pt-32 pb-60">
+        <div className="font-arimo mx-36 ">
+          <h1 className="font-bold text-5xl tracking-wide leading-relaxed">
+            Intro
+          </h1>
           <br />
-          <p className="text-lg">
+          <p className="text-lg tracking-wide leading-relaxed">
             Parallax effects can add that extra spark to your website. That can
             add an extra element of dynamics that draw in the use - making your
             website more fun, inticing, eye-catching sometimes even tacky. While
@@ -28,6 +31,35 @@ const Content = () => {
             you care about accesibility when using parrallx and how can you use
             it to creater a fun dynamic website that has little to ne
             interferance with accesibility needs. Let's dive in!
+          </p>
+        </div>
+      </div>
+      <Parallax speed={20}>
+        <img className="w-full -mt-44 z-2" src={light_tear} alt="paper tear" />
+        <div className=" p-28 bg-white" />
+      </Parallax>
+      <div className="relative z-3 -mt-44 pt-32 pb-60">
+        <div className="font-arimo mx-36">
+          <h1 className="font-bold text-5xl tracking-wide leading-relaxed">
+            Impact on Accessibility
+          </h1>
+          <br />
+          <p className="text-lg tracking-wide leading-relaxed">
+            Imagine you already struggle with reading text, do you think that
+            the experience of reading is made easier when the text is on square
+            that flies past your vision at a completely different speed than
+            your expecting - it makes the text more difficult to read. What if
+            you get overwhelmed by too much stimuli? Having a flurry of moving
+            images whizz past you in a flash of color could be completely
+            overwhelming. If you struggle with concentration and you're
+            bombarded with a continuously changing fluctuation of items on your
+            screen - spinning, scrolling, fading in and out - it's going to be
+            pretty hard to tell where to look or how to use the page. And these
+            aren't just problems that effect neurodivergent or disabled people,
+            using parallax effects have the potential to turn away many
+            different users based on the fact that while they might look
+            "dynamic" they can have a negative impact on the functionality of
+            your website.
           </p>
         </div>
       </div>
