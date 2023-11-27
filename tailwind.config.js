855a629f2bf9b273e4ec38300030224d072a926e
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-27 16:31:41 +0000

    Imported and setup custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        arimo: ["Arimo", "sans-serif"],
+        libre: ["Libre Baskerville", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
