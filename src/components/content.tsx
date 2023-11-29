3ad0ba09ff75baffa12c40815a286e29c579cf68
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-11-29 18:52:32 +0000

    updated content
    added lists and new content section:  Accessibility with Parallax?

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -10,7 +10,7 @@ const Content = () => {
         <div className="p-28 bg-black" />
       </Parallax>
 
-      <div className="relative bg-black text-white z-1 -mt-40 pt-32 pb-80">
+      <div className="relative bg-black text-white z-1 -mt-40 pt-32 pb-72">
         <div className="font-arimo mx-36 ">
           <h1 className="font-bold text-5xl tracking-wide leading-relaxed">
             Intro
@@ -31,26 +31,90 @@ const Content = () => {
         <img className="w-full -mt-44 z-2" src={light_tear} alt="paper tear" />
         <div className=" p-28 bg-white" />
       </Parallax>
-      <div className="relative z-3 -mt-44 pt-32 pb-60">
+      <div className="relative z-3 -mt-44 pt-32 pb-72">
         <div className="font-arimo mx-36">
           <h1 className="font-bold text-5xl tracking-wide leading-relaxed">
             Impact on Accessibility
           </h1>
           <br />
           <p className="text-lg tracking-wide leading-relaxed">
-            Imagine you find reading challenging and you suddenly have text
-            flying past at a completely different speed than your expecting - it
-            makes even more difficult to read. What if you get overwhelmed by
-            too much stimuli? Having a flurry of moving images whizz past you in
-            a flash of color could be completely overwhelming. If you struggle
-            with concentration and you're bombarded with a continuously changing
-            fluctuation of items on your screen - spinning, scrolling, fading in
-            and out - it's going to be pretty hard to tell where to look or how
-            to use the page. And these aren't just problems that effect
-            neurodivergent and disabled people, using parallax effects has the
-            potential to turn away many different users based on the fact that
-            while they might look "dynamic" they can have a negative impact on
-            the functionality of your website.
+            We all know that there are many elements of parallax effects that
+            can negatively impact the usability of a website. But moving content
+            at varying speeds can have particular repercussion on users with
+            accessibility needs.
+            <br />
+            <br />
+            <ul className="list-disc ml-10">
+              <li className="mb-3">
+                Imagine you find reading challenging and you suddenly have text
+                flying past at a completely different speed than your expecting
+                - it makes even more difficult to read.
+              </li>
+              <li className="mb-3">
+                What if you get overwhelmed by too much stimuli? Having a flurry
+                of moving images whizz past you in a flash of color could be
+                completely overwhelming.
+              </li>
+              <li>
+                If you struggle with concentration and you're bombarded with a
+                continuously changing fluctuation of items on your screen, it's
+                going to be pretty hard to tell where to focus or even how to
+                navigate the page.
+              </li>
+            </ul>
+            <br />
+            These are just a few examples of the issues parallax can cause and
+            these problems don't just effect neurodivergent and disabled people.
+            Using these visual effects has the potential to turn away many
+            different users as while they might look "dynamic" they can have a
+            negative impact on the aesthetic functionality of your website.
+          </p>
+        </div>
+      </div>
+      <Parallax speed={20}>
+        <img className="w-full -mt-44 z-3" src={dark_tear} alt="paper tear" />
+        <div className=" p-28 bg-black" />
+      </Parallax>
+      <div className="relative bg-black z-5 -mt-44 pt-32 pb-72">
+        <div className="font-arimo text-white mx-36">
+          <h1 className="font-bold text-5xl tracking-wide leading-relaxed">
+            Accessibility with Parallax?
+          </h1>
+          <br />
+          <p className="text-lg tracking-wide leading-relaxed">
+            While there are aspects of the effects that impact accessibility,
+            there are still many ways to utilize parallax to create a dynamic,
+            interactive website that doesn't interfere with access to content.
+            <br />
+            <br />
+            <ul className="list-disc ml-10">
+              <li className="mb-3">
+                Take it slow. Try to have your elements moving fast enough that
+                you get the effect but not much faster. There should be a sense
+                of movement rather than a clear rush of motion.
+              </li>
+              <li className="mb-3">
+                Keep parallax effects scrolling at the same rate throughout
+                their path. For instance if you're using a library like
+                react-scroll-parallax, avoid using "easing". When you change the
+                speed of scrolling mid way through the element's path it can be
+                extremely jarring.
+              </li>
+              <li className="mb-3">
+                Avoid putting text on parallax elements. Text should always
+                scroll at the same rate as the user. In this webpage, for
+                instance, the text is separate to all parallax scrolling
+                elements. This give a subtle dynamic feel to the page while not
+                interfering with the content.
+              </li>
+              <li>
+                And of course, provide a clear way to disable animations and
+                effects. Give the user control of their experience.
+              </li>
+            </ul>
+            <br />
+            These are just some ideas that will help when coding and designing
+            parallax websites with accessibility in mind.
           </p>
         </div>
       </div>
