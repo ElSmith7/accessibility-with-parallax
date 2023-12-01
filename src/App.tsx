38b0d970b542575e93fa7cdf0c3a386a5116df87
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-12-01 17:05:00 +0000

    Added NavBar to App
    created state for enabling and disabling parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ParallaxProvider } from "react-scroll-parallax";
+import { useState } from "react";
 import Hero from "./components/hero";
 import Content from "./components/content";
+import NavBar from "./components/nav-bar";
 
 const App = () => {
+  const [parallaxEnabled, setParallaxEnabled] = useState(true);
+
   return (
     <ParallaxProvider>
-      <Hero />
-      <Content />
+      <NavBar toggleParallax={() => setParallaxEnabled((prev) => !prev)} />
+      <Hero parallaxEnabled={parallaxEnabled} />
+      <Content parallaxEnabled={parallaxEnabled} />
     </ParallaxProvider>
   );
 };
