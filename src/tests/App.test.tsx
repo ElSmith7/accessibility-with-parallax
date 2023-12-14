c79de4f4cb7abe6dc67a02b3283dfdd5bc504e95
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-12-14 16:25:24 +0000

    Added Resize Observer mock and tested parallax
    Tested that Parallax div was receiving the "parallaxEnabled" state and updating the "transform" styling

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,6 +3,14 @@ import "@testing-library/jest-dom";
 import App from "../App";
 import "react-scroll-parallax";
 
+class MockResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.ResizeObserver = MockResizeObserver as any;
+
 describe("App Component", () => {
   it("renders NavBar, Hero, Content, and Footer components", () => {
     const { getByRole } = render(<App />);
@@ -10,5 +18,28 @@ describe("App Component", () => {
     expect(getByRole("banner")).toBeInTheDocument();
     expect(getByRole("main")).toBeInTheDocument();
     expect(getByRole("contentinfo")).toBeInTheDocument();
+  });
+
+  it("toggles parallaxEnabled state when NavBar toggle is clicked", () => {
+    render(<App />);
+    const toggleButton = screen.getByRole("button", {
+      name: /parallax is on/i,
+    });
+
+    const parallaxImages = screen.getAllByAltText(
+      /crumpled paper sketch|typewriter sketch|paper tear/
+    );
+    const parallaxDivBeforeToggle = parallaxImages[0].parentNode;
+
+    expect(parallaxDivBeforeToggle).toHaveStyle(
+      "transform: translateY(19.999999999999996px)"
+    );
+
+    fireEvent.click(toggleButton);
+
+    const parallaxDivAfterToggle = parallaxImages[0].parentNode;
+    expect(parallaxDivAfterToggle).not.toHaveStyle(
+      "transform: translateY(19.999999999999996px)"
+    );
   });
 });
